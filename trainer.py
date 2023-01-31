8436c7109c8f36964ce4d9e9c4be2615d9c862b5
dahan-1996 <61272044+dahan-1996@users.noreply.github.com>
2023-01-31 17:50:17 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -4,14 +4,18 @@ import numpy as np
 import skimage.io
 import tensorflow as tf
 from config import global_variables as gl
-from utils.common_utils import parse_example, get_file_list, MyMIOU, dice_coef,dice_loss,dice_ce,dice_omit_ce,ce
+from utils.common_utils import parse_example, get_file_list, MyMIOU, dice_coef,dice_loss,dice_ce,dice_omit_ce,ce,sub_loss,sub_loss2
 from networks.hrnetV2 import hrnet_v2, hrnet18_v2, hrnet32_v2, hrnet48_v2
 from networks.unet import NestedUNet, U_Net,AttU_Net
+
+from torch.utils.tensorboard import SummaryWriter
+
+#writer = SummaryWriter('./Result')   # 
 
 class SegNet:
     def __init__(self):
         self.n_class = gl.CLASSES
-        self.loss_type = 'dice_ce'
+        self.loss_type = 'dice'
         self.optimizer_name = 'adam'
         self.pretrained_ckpt_dir = ''
         self.batch_size = gl.BATCH_SIZE
@@ -29,11 +33,12 @@ class SegNet:
 
     def build_model(self):
         x = tf.keras.layers.Input(shape=gl.RESIZED_IMG)
+        #out= NestedUNet(class=16)
         out = hrnet32_v2(x, n_class=self.n_class, include_top=True, mode="seg")  # mode = seg > hrnet v2 + semantic segmentation, clsf > hrnet v2 + classifier, ocr > hrnet v2 + ocr + semantic segmentation
-        # print(out.shape)
-        out = tf.keras.layers.UpSampling2D((4, 4))(out)
-        model = tf.keras.Model(x, out)
-
+        #print(out.shape)
+        #out = tf.keras.layers.UpSampling2D((4, 4))(out)
+        #model = tf.keras.Model(x, out)
+        model=NestedUNet(classes=gl.CLASSES)
         # model = AttU_Net(classes=gl.CLASSES)
         # # model.build((None, *gl.RESIZED_IMG))
         return model
@@ -47,6 +52,10 @@ class SegNet:
             loss = dice_loss
         elif self.loss_type == 'dice_ce':
             loss = dice_ce
+        elif self.loss_type=='sub_loss':
+            loss= sub_loss
+        elif self.loss_type=='sub_loss2':
+            loss= sub_loss2
         else:
             loss = dice_omit_ce
         return loss
@@ -65,50 +74,74 @@ class SegNet:
         return opt
 
     def fit(self):
+        
         model = self.build_model()
         loss = self.get_loss()
         opt = self.get_optimizer()
         monitor=tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, mode='auto')
-        saver = tf.keras.callbacks.ModelCheckpoint(filepath='./ckpt/', save_weights_only=True,save_best_only=True)
+        
+        #saver = tf.keras.callbacks.ModelCheckpoint(filepath="/home/students/dahan/miccai_baidu_cv_2022-master/ckpt/dice/checkpoint" ,save_weights_only=False, save_best_only=True)
         model.compile(loss=loss, optimizer=opt, metrics=[dice_coef, MyMIOU(num_classes=4)])
-
 
         train_set = self.prepare_input(gl.TRAIN_DIR)
         val_set = self.prepare_input(gl.VAL_DIR)
-        model.fit(x=train_set, y=None, epochs=self.epoch, validation_data=val_set,callbacks=[monitor,saver])
+        model.fit(x=train_set, y=None, epochs=5)
+        model.save("my_model.h5")
+        new_model = tf.keras.models.load_model('my_model.h5')
+        new_model.summary()
+        #loss, acc = new_model.evaluate(test_images, test_labels, verbose=2)
+        #print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
+        #model.fit(x=train_set, y=None, epochs=self.epoch, validation_data=val_set,callbacks=[monitor,saver])
 
+        
     def predict(self,load_from_weights=True, save_predictions=False):
+        
         test_set = self.prepare_input(gl.TEST_DIR,split='test')
         if load_from_weights:
             model = self.build_model()
             model.compile()
-            model.load_weights('./ckpt/dice/')
+            model.built=True
+            model.load_weights("/home/students/dahan/miccai_baidu_cv_2022-master/ckpt/dicece/checkpoint")
         else:
             dependencies = {
                 'dice_coef': dice_coef,
-                'my_miou':MyMIOU(num_classes=4)
+                'my_miou':MyMIOU(num_classes=4),
+                'New_loss':dice
+                #'New_loss':sub_loss2
             }
 
-            model = tf.keras.models.load_model('./ckpt/',custom_objects=dependencies)
+            model = tf.keras.models.load_model("/home/students/dahan/miccai_baidu_cv_2022-master/ckpt/dice/checkpoint",custom_objects=dependencies)
         out = model.predict(x=test_set,verbose=2)
+        #print(out)
+        #breakpoint()
         predict = np.argmax(out, axis=-1)
         predict[predict==0] = 255
         predict[predict==1] = 0
         predict[predict==2] = 80
-        predict[predict==3] = 160
+        predict[predict==3] = 160  
         predict = predict.astype(np.float32)
+
 
         if save_predictions:
             for i in range(predict.shape[0]):
                 curr = cv2.resize(predict[i], gl.ORIGINAL_IMG_SIZE)
+                #print(curr)
+                #print("/home/students/dahan/miccai_baidu_cv_2022-master/res/0%d.png" % (predict.shape[0]+i+1))
+                #skimage.io.imsave("/home/students/dahan/miccai_baidu_cv_2022-master/res/0%d.png" % (predict.shape[0]+i+1),curr)
                 cv2.imwrite('./res/0%d.png' % (predict.shape[0]+i+1), curr)
 
 
 
+
+            
 def main():
     net = SegNet()
     net.fit()
-    net.predict(load_from_weights=True,save_predictions=True)
+    #net.predict(load_from_weights=True,save_predictions=True)
+    
+    
+    
+    
 
 if __name__ == '__main__':
     main()
